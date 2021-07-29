c3c0414d1cbf2fa3ccfc81682cf5581c5fca0310
forkbabu <sayantandas30011998@gmail.com>
2021-07-30 03:19:06 +0530

    Update set2.sh

--- set2.sh
+++ set2.sh
@@ -1,5 +1,4 @@
 nohup bundle exec jekyll serve -P 4000 > output.txt &
 nohup ./ngrok http 4000 &
-sudo apt-get install jq
 curl -s http://localhost:4040/api/tunnels/command_line | jq -r --unbuffered '.public_url' > url.txt
 cat url.txt
