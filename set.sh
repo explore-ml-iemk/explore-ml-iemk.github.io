7a72f9185eb289c4bfe021f4c760667c50b599b2
forkbabu <sayantandas30011998@gmail.com>
2021-07-30 01:45:25 +0530

    Update set.sh

--- set.sh
+++ set.sh
@@ -4,6 +4,6 @@ bundle install
 bundle exec jekyll serve -P 4000 &
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
-./ngrok authtoken $NGROK_TOKEN
+./ngrok authtoken "$NGROK_TOKEN"
 curl -s http://localhost:4040/api/tunnels/command_line | jq -r --unbuffered '.public_url' > url.txt
 cat url.txt
