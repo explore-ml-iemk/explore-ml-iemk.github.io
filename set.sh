df94d255af51a747968d4e1362e17c0379f6276d
forkbabu <sayantandas30011998@gmail.com>
2021-07-30 02:00:18 +0530

    Update set.sh

--- set.sh
+++ set.sh
@@ -1,9 +1,10 @@
 sudo apt-get install ruby-full build-essential zlib1g-dev -y
 sudo gem install jekyll bundler
 bundle install
-bundle exec jekyll serve -P 4000 &
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
 ./ngrok authtoken "$NGROK_TOKEN"
+bundle exec jekyll serve -P 4000 &
+./ngrok http 4000
 curl -s http://localhost:4040/api/tunnels/command_line | jq -r --unbuffered '.public_url' > url.txt
 cat url.txt
